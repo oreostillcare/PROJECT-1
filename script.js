2e97c11f1497fc07fb74ebe608dcfd4217ab3b02
Josh Andrie Maralit <joshandriemaralit@gmail.com>
2025-02-05 00:27:57 +0800

    pagod na ang nigga

--- script.js
+++ script.js
@@ -8,5 +8,26 @@ var github_button_3 = document.querySelector("#github_button_3");
 
 
 visit_button_1.onclick = function() {
-    window.open('https://pornhub.com');
+    window.open('https://');
+}
+
+visit_button_2.onclick = function() {
+    window.open('https://');
+}
+
+visit_button_3.onclick = function() {
+    window.open('https://');
+}
+
+
+github_button_1.onclick = function() {
+    window.open('https://');
+}
+
+github_button_2.onclick = function() {
+    window.open('https://');
+}
+
+github_button_3.onclick = function() {
+    window.open('https://');
 }
