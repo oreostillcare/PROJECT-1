444759cbb7e8291b4cda7ea8a785ddc32acace20
Josh Andrie Maralit <joshandriemaralit@gmail.com>
2025-02-04 23:39:46 +0800

    pagod na

---
+++ script.js
@@ -0,0 +1,12 @@
+var visit_button_1 = document.querySelector("#visit_project_button_1");
+var visit_button_2 = document.querySelector("#visit_project_button_2");
+var visit_button_3 = document.querySelector("#visit_project_button_3");
+
+var github_button_1 = document.querySelector("#github_button_1");
+var github_button_2 = document.querySelector("#github_button_2");
+var github_button_3 = document.querySelector("#github_button_3");
+
+
+visit_button_1.onclick = function() {
+    window.open('https://pornhub.com');
+}
