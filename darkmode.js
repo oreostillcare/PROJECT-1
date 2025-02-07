bb87bf719ed153736308b56df8243af465d41d7f
Josh Andrie Maralit <joshandriemaralit@gmail.com>
2025-02-07 23:26:48 +0800

    update

--- darkmode.js
+++ darkmode.js
@@ -7,14 +7,42 @@ var enableDarkMode = function() {
 
     themeSwitch.querySelector("svg:first-child").style.display = 'none';
     themeSwitch.querySelector("svg:last-child").style.display = 'block';
-}
+
+    document.querySelector(".emaillogo").style.display = 'none';
+    document.querySelector(".emaildarkcontact").style.display = 'inline';
+
+    document.querySelectorAll(".githublogo, .linkedinlogo").forEach((logo) => {
+        if (logo.classList.contains("dark")) {
+            logo.style.display = 'inline'; 
+        } else {
+            logo.style.display = 'none'; 
+        }
+    });
+};
 
 var disableDarkMode = function() {
     document.body.classList.remove('darkmode');
     localStorage.removeItem('darkmode');
- 
+
     themeSwitch.querySelector("svg:first-child").style.display = 'block';
     themeSwitch.querySelector("svg:last-child").style.display = 'none';
+
+    document.querySelector(".emaillogo").style.display = 'inline';
+    document.querySelector(".emaildarkcontact").style.display = 'none';
+
+    document.querySelectorAll(".githublogo, .linkedinlogo").forEach((logo) => {
+        if (logo.classList.contains("dark")) {
+            logo.style.display = 'none';
+        } else {
+            logo.style.display = 'inline'; 
+        }
+    });
+};
+
+if (darkmode === 'active') {
+    enableDarkMode();
+} else {
+    disableDarkMode();
 }
 
 themeSwitch.onclick = function() {
@@ -24,4 +52,4 @@ themeSwitch.onclick = function() {
     } else {
         disableDarkMode();
     }
-}
+};
